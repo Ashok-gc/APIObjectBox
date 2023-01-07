1103dff5daf55656361c9ac014c80fb563d5f839
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-01-07 22:04:12 +0545

    api for object box

---
+++ controllers/user_controller.js
@@ -0,0 +1,39 @@
+const User = require("../models/User")
+
+const registeruser =  ((req,res,next)=>{
+    User.findOne({username : req.body.username})
+    .then((user)=>{
+        if(user != null){
+
+            let err =  new Error(`user ${req.body.username} already exists`)
+            res.status(400)
+            return next(err)
+        }
+        else{
+           user =  new User(req.body)
+           user.save().then((user)=>{
+            res.json(user)
+           })
+           .catch(next)
+           
+        }
+    })
+   .catch(next)
+
+})
+
+const loginuser = ((req,res,next)=>{
+    User.findOne({username : req.body.username,password:req.body.password}).then(user=>{
+        if(user == null){
+            res.status(404)
+            let err =  new error(`User ${req.body.username} doesnot exist`)
+            return next(err)
+        }
+        else{
+             
+            res.json({"user " :user,"reply ": "Logged in sucessfully"})
+        }
+})
+})
+
+module.exports =  {registeruser,loginuser}
---
+++ models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+const Userschema =  mongoose.Schema({
+
+    firstname : {
+        type : String,
+        required : [true,"Firstname is Required"]
+    },
+    lastname  : {
+        type : String,
+        required : [true,"Lastname is Required"]
+    },
+    batch : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Batch"
+    },
+    course : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "Course"
+    }],
+    username :{
+        type : String,
+        required : [true , "Username is Required"]
+    },
+    password : {
+        type : String,
+        
+    },
+    profilepicture :{
+        type:String
+    }
+})
+module.exports =  mongoose.model("User",Userschema)
+
